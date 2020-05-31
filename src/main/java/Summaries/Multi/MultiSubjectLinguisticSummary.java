fececab8761daf8a5363459637cb138a94723f72
Motwg <38580353+Motwg@users.noreply.github.com>
2020-05-31 10:56:00 +0200

    Added t2 - t11 to MultiSubjectLinguisticSummary

--- src/main/java/Summaries/Multi/MultiSubjectLinguisticSummary.java
+++ src/main/java/Summaries/Multi/MultiSubjectLinguisticSummary.java
@@ -1,5 +1,8 @@
 package Summaries.Multi;
 
+import java.util.List;
+
 public interface MultiSubjectLinguisticSummary {
-    double t();
+    double t1();
+    double overallT(List<Double> weights);
 }
